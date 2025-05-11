1801e84fb1301cb8acdb877b36ba13c93eb9fd3a
Maria-Ezzat <125491036+MariaEzat@users.noreply.github.com>
2025-05-11 14:19:05 +0300

    Update Navbar

--- src/app/features/sites/layout/layout.component.ts
+++ src/app/features/sites/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
-
+  openDropdown: string | null = null;
+  isNavbarCollapsed = true;
+  toggleDropdown(menu: string) {
+    this.openDropdown = this.openDropdown === menu ? null : menu;
+  }
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const clickedElement = event.target as HTMLElement;
+    if (!clickedElement.closest('.dropdown')) {
+      this.openDropdown = null;
+    }
+  }
 }
