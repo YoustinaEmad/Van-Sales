ac0a0f76e0195309acf361fddfdf963a929af2c8
Youdtina-KOG <y.emad@Kog-eg.com>
2025-05-15 16:12:44 +0300

    Make get productById

--- src/app/features/sites/Product/components/product-details/product-details.component.ts
+++ src/app/features/sites/Product/components/product-details/product-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { productCreateViewModel, productViewModel } from '../../interfaces/product';
+import { productCreateViewModel, productDetailsVM, productViewModel } from '../../interfaces/product';
 import { ApiService } from 'src/app/shared/service/api.service';
 import { ProductService } from '../../service/product.service';
 
@@ -10,7 +10,7 @@ import { ProductService } from '../../service/product.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
-product: productCreateViewModel | null = null;
+product: productDetailsVM | null = null;
 
   constructor(private route: ActivatedRoute, private _apiService: ApiService,private _ProductService:ProductService) {}
 
