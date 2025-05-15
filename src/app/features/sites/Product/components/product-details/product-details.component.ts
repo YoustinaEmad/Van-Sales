95d1f1877f8e9c65aba91d979443c21d1150bd41
Youdtina-KOG <y.emad@Kog-eg.com>
2025-05-15 16:16:16 +0300

    get unit and grade name

--- src/app/features/sites/Product/components/product-details/product-details.component.ts
+++ src/app/features/sites/Product/components/product-details/product-details.component.ts
@@ -13,7 +13,15 @@ export class ProductDetailsComponent {
 product: productDetailsVM | null = null;
 
   constructor(private route: ActivatedRoute, private _apiService: ApiService,private _ProductService:ProductService) {}
-
+ Grades = [
+    { id: 1, name: 'HighGrade ' },
+    { id: 2, name: 'LowGrade ' },
+  ];
+  Units = [
+    { id: 1, name: 'Cartoon  ' },
+    { id: 2, name: 'Drum   ' },
+    { id: 3, name: 'Pail   ' },
+  ]
  ngOnInit(): void {
   const id = this.route.snapshot.paramMap.get('id');
   if (id) {
@@ -22,5 +30,17 @@ product: productDetailsVM | null = null;
     });
   }
 }
+
+
+
+ getUnitName(unitId: number): string {
+    const unit = this.Units.find(u => u.id === unitId);
+    return unit ? unit.name.trim() : '';
+  }
+
+  getGradeName(gradeId: number): string {
+    const grade = this.Grades.find(g => g.id === gradeId);
+    return grade ? grade.name.trim() : '';
+  }
 
 }
