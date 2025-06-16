fbdcab574ede0e4ddb78d5804eb97ed9626d5f64
Maria-Ezzat <m.wanis@kog-eg.com>
2025-06-16 12:17:15 +0300

    Edit in Navbar

--- src/app/features/sites/layout/layout.component.ts
+++ src/app/features/sites/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -6,11 +8,27 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
+
+  constructor(public router: Router) {
+    // اشتراك في التنقلات
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentUrl = event.urlAfterRedirects;
+      });
+
+    // تعيين URL في البداية
+    this.currentUrl = this.router.url;
+  }
+
   openDropdown: string | null = null;
   isNavbarCollapsed = true;
+  currentUrl: string = '';
+
   toggleDropdown(menu: string) {
     this.openDropdown = this.openDropdown === menu ? null : menu;
   }
+
   @HostListener('document:click', ['$event'])
   onClickOutside(event: MouseEvent) {
     const clickedElement = event.target as HTMLElement;
@@ -18,4 +36,24 @@ export class LayoutComponent {
       this.openDropdown = null;
     }
   }
+
+  closeDropdown() {
+    this.openDropdown = null;
+  }
+
+  isLocationActive(): boolean {
+    return this.currentUrl.includes('/sites/governorates') || this.currentUrl.includes('/sites/cities');
+  }
+  isItemsActive(): boolean {
+    return this.currentUrl.includes('/sites/category') ||
+      this.currentUrl.includes('/sites/product') ||
+      this.currentUrl.includes('/sites/productGroup') ||
+      this.currentUrl.includes('/sites/brand');
+  }
+  isSNOActive(): boolean {
+    return this.currentUrl.includes('/sites/transfers') ||
+      this.currentUrl.includes('/sites/transferSalesManToSalesMan') ||
+      this.currentUrl.includes('/sites/transferWarehouseToSalesMan') 
+  }
+
 }
