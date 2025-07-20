5e48a675b71db8be1c1233ffb5b1c281531204a9
Youdtina-KOG <y.emad@Kog-eg.com>
2025-07-20 15:36:42 +0300

    make get warehouseNames in warehouse details

--- src/app/features/sites/warehouse/interfaces/warehouse-view-model.ts
+++ src/app/features/sites/warehouse/interfaces/warehouse-view-model.ts
@@ -69,7 +69,7 @@ export class warehouseDetailsViewModel {
   longitude: string;
   buildingData: string;
   warehouseProducts: WarehouseProductViewModel[];
-
+warehouseNames:[]
 }
 
 export class WarehouseProductViewModel {
