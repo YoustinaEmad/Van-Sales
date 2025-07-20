fb88471c13ea16c49fe72170d17628751eb12681
Maria-Ezzat <125491036+MariaEzat@users.noreply.github.com>
2025-07-20 11:56:13 +0300

    Make brand based On products in Invoice

--- src/app/features/sites/invoice/interface/invoice-view-model.ts
+++ src/app/features/sites/invoice/interface/invoice-view-model.ts
@@ -24,42 +24,49 @@ export class IvoiceSearchViewModel {
 
 export class InvoiceCreateViewModel {
     id: string
-    clientID: string ;
-    salesManID?:string;
-    notes?:string;
-    invoiceDetails:SellingInvoicesDetailsVM[];
+    clientID: string;
+    salesManID?: string;
+    notes?: string;
+    invoiceDetails: SellingInvoicesDetailsVM[];
 }
 
 export class SellingInvoicesDetailsVM {
-    productId:string;
-    itemWeightPerKG:number;
-    quantity:number;
-    itemPrice:number;
+    productId: string;
+    itemWeightPerKG: number;
+    quantity: number;
+    itemPrice: number;
 }
 
 export class InvoiceDetailsViewModel {
     id: string;
     invoiceNumber: string;
-    clientID : string;
-    clientName : string;
-   salesManID : string;
-   salesManName : string;
-   totalPrice : number;
-   totalNetPrice : number;
-   totalWeightInKG : number;
-   totalQuantity : number;
-   taxAmount : number;
-   sellingInvoicesDetails : SellingInvoicesDetailsVM2[];
+    clientID: string;
+    clientName: string;
+    salesManID: string;
+    salesManName: string;
+    totalPrice: number;
+    totalNetPrice: number;
+    totalWeightInKG: number;
+    totalQuantity: number;
+    taxAmount: number;
+    sellingInvoicesDetails: SellingInvoicesDetailsVM2[];
 }
 
 
 export class SellingInvoicesDetailsVM2 {
-productId : string;
-productName : string;
-quantity : number;
-itemPrice : number;
-price : number;
-itemWeightPerKG : number;
-weightPerKG : number;
-unit : number;
+    productId: string;
+    productName: string;
+    quantity: number;
+    itemPrice: number;
+    price: number;
+    itemWeightPerKG: number;
+    weightPerKG: number;
+    unit: number;
 }
+
+export class SelectBrandList {
+    iD: string;
+    name: string;
+    path: number;
+}
+
